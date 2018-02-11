13c0cea77fc8bac7d3139958e0c5cda81fe439c5
freelanseod <free-lanse516@list.ru>
2018-02-11 14:53:34 +0300

    second commit

---
+++ testbase/src/test/java/pages/LandingPage.java
@@ -0,0 +1,29 @@
+package pages;
+
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Selectors.byLinkText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class LandingPage {
+
+    public SelenideElement
+            loginMainButton = $(byLinkText("Login")),
+     //       profileLink = $x("//div[./a[@href = '/@izyaz']]"),
+            linkOnCollections=$("[data-test='user-nav-link-collections']");
+
+    public LandingPage() {
+        open("/");
+    }
+
+    public LoginPage clickOnLoginButton() {
+        loginMainButton.click();
+        return new LoginPage();
+    }
+
+    public ProfilePage clickOnProfileLink() {
+      actions().click($("#app > div > header > nav > div.aApNa > div.ODWzM._1N39c > a")).build().perform();
+    return new ProfilePage();
+    }
+
+}
