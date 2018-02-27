a0f35d9268297710a3ff873e4b34c8d83e9c2a70
freelanseod <free-lanse516@.list.ru>
2018-02-27 22:38:52 +0300

    pain

--- src/test/java/SignInTest.java
+++ src/test/java/SignInTest.java
@@ -31,7 +31,7 @@ public class SignInTest extends BaseTest {
 
     @Test
     public void userNotFound() {
-        loginPage.fillUserInfoField("16@list.ru", "123456");
+        loginPage.fillUserInfoField("16@list.ru", "123456789");
         loginPage.clickonLoginButton();
         loginPage.checkThatUserNotLogin();
 
